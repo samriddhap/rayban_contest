c9dce821cb6f2918cf1911d835b9c731b48226d7
Samriddha Prajapati <warnersam74@gmail.com>
2017-08-07 14:30:47 +0530

    Made the blog section
    added the blogs section to the main page

---
+++ blog.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Ray-Ban | Game Zone </title>
+
+    <meta name="description" content="Ray-Ban - Community contest platform">
+    <meta name="keywords" content="">
+    <meta name="author" content="_nK">
+
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- START: Styles -->
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7cOpen+Sans:400,700" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
+
+    <!-- IonIcons -->
+    <link rel="stylesheet" href="assets/bower_components/ionicons/css/ionicons.min.css">
+
+    <!-- Flickity -->
+    <link rel="stylesheet" href="assets/bower_components/flickity/dist/flickity.min.css">
+
+    <!-- Photoswipe -->
+    <link rel="stylesheet" type="text/css" href="assets/bower_components/photoswipe/dist/photoswipe.css">
+    <link rel="stylesheet" type="text/css" href="assets/bower_components/photoswipe/dist/default-skin/default-skin.css">
+
+    <!-- Seiyria Bootstrap Slider -->
+    <link rel="stylesheet" href="assets/bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css">
+
+    <!-- Summernote -->
+    <link rel="stylesheet" href="assets/bower_components/summernote/dist/summernote.css">
+
+    <!-- Prism -->
+    <link rel="stylesheet" type="text/css" href="assets/bower_components/prism/themes/prism-tomorrow.css">
+
+    <!-- GoodGames -->
+    <link rel="stylesheet" href="assets/css/goodgames.min.css">
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="assets/css/custom.css">
+    
+    <!-- END: Styles -->
+
+    <!-- jQuery -->
+    <script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+
+    
+</head>
+
+
+<!--
+    Additional Classes:
+        .nk-page-boxed
+-->
+<body>
+ 
+<!--
+    Additional Classes:
+        .nk-header-opaque
+-->
+<header class="nk-header nk-header-opaque">
+    <!--
+        START: Navbar
+
+        Additional Classes:
+            .nk-navbar-sticky
+            .nk-navbar-transparent
+            .nk-navbar-autohide
+    -->
+    <?php include 'header.php';?>
+    <!-- END: Navbar -->
+
+</header>
+   
+        <!--
+    START: Navbar Mobile
+
+    Additional Classes:
+        .nk-navbar-left-side
+        .nk-navbar-right-side
+        .nk-navbar-lg
+        .nk-navbar-overlay-content
+-->
+<div id="nk-nav-mobile" class="nk-navbar nk-navbar-side nk-navbar-right-side nk-navbar-overlay-content hidden-lg-up">
+    <div class="nano">
+        <div class="nano-content">
+            <a href="home.php" class="nk-nav-logo">
+                <img src="assets/images/logo.png" alt="" width="120">
+            </a>
+            <div class="nk-navbar-mobile-content">
+                <ul class="nk-nav">
+                    <!-- Here will be inserted menu from [data-mobile-menu="#nk-nav-mobile"] -->
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END: Navbar Mobile -->
+
+    
+
+    <div class="nk-main">
+        
+            <!-- START: Breadcrumbs -->
+<div class="nk-gap-1"></div>
+<div class="container">
+    <ul class="nk-breadcrumbs">
+         <li><a href="#">Latest</a></li>
+         <li><span>Blog Posts</span></li>
+        
+    </ul>
+</div>
+<div class="nk-gap-1"></div>
+<!-- END: Breadcrumbs -->
+        
+<div class="container">
+    <!-- START: Categories -->
+    <div class="nk-popup-gallery">
+                <div class="row vertical-gap">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="nk-gallery-item-box">
+                            <a href="assets/images/blog3.jpg" class="nk-gallery-item" data-size="1016x572">
+                                <div class="nk-gallery-item-overlay"><span class="ion-eye"></span></div>
+                                <img src="assets/images/blog3.jpg" height="200px" width="150px" alt="">
+                            </a>
+                            <div class="nk-gallery-item-description">
+                                Fulll posts goes here.Fulll posts goes here.Fulll posts goes here.Fulll posts goes here.Fulll posts goes hereFulll posts goes here.Fulll posts goes here.Fulll posts goes here.Fulll posts goes here.Fulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes here.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="nk-gallery-item-box">
+                            <a href="assets/images/blog.jpg" class="nk-gallery-item" data-size="1188x594">
+                                <div class="nk-gallery-item-overlay"><span class="ion-eye"></span></div>
+                                <img src="assets/images/blog.jpg" height="200px" width="150px"  alt="">
+                            </a>
+                            <div class="nk-gallery-item-description">Fulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereorning fill. Living so second darkness. Moveth were male. May creepeth. Be tree fourth.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="nk-gallery-item-box">
+                            <a href="assets/images/blog2.jpg" class="nk-gallery-item" data-size="625x350">
+                                <div class="nk-gallery-item-overlay"><span class="ion-eye"></span></div>
+                                <img src="assets/images/blog2.jpg" height="200px" width="150px"  alt="">
+                            </a>
+                            <div class="nk-gallery-item-description">Fulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereFulll posts goes hereorning fill. Living so second darkness. Moveth were male. May creepeth. Be tree fourth.
+                            </div>
+                     
+                        </div>
+                    </div>
+
+                    <!--<div class="col-lg-4 col-md-6">
+                        <div class="nk-gallery-item-box">
+                            <a href="assets/images/lg-sb6.jpg" class="nk-gallery-item" data-size="873x567">
+                                <div class="nk-gallery-item-overlay"><span class="ion-eye"></span></div>
+                                <img src="assets/images/sunburn4.jpg" alt="">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="nk-gallery-item-box">
+                            <a href="assets/images/lg-sb11.jpg" class="nk-gallery-item" data-size="471x269">
+                                <div class="nk-gallery-item-overlay"><span class="ion-eye"></span></div>
+                                <img src="assets/images/sunburn5.jpg" alt="">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="nk-gallery-item-box">
+                            <a href="assets/images/lg-sb14.jpg" class="nk-gallery-item" data-size="472x438">
+                                <div class="nk-gallery-item-overlay"><span class="ion-eye"></span></div>
+                                <img src="assets/images/sunburn6.jpg" alt="">
+                            </a>
+                        </div>
+                    </div>-->
+                </div>
+            </div>
+    <!-- END: Categories -->
+    <div class="nk-gap-2"></div>
+    <div class="nk-gap-2"></div>
+    <div class="nk-gap-2"></div>
+    <!-- END: Most Popular -->
+</div>
+
+<div class="nk-gap-2"></div>
+<!-- START: Footer -->
+<?php include 'footer.php';?> 
+<!-- END: Footer -->
+
+        
+</div>
+<!-- START: Page Background -->
+
+    <img class="nk-page-background-top" src="assets/images/bg-top-4.png" alt="">
+    <img class="nk-page-background-bottom" src="assets/images/bg-bottom.png" alt="">
+
+<!-- END: Page Background -->
+
+         
+<!-- START: Scripts -->
+
+<!-- GSAP -->
+<script src="assets/bower_components/gsap/src/minified/TweenMax.min.js"></script>
+<script src="assets/bower_components/gsap/src/minified/plugins/ScrollToPlugin.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="assets/bower_components/tether/dist/js/tether.min.js"></script>
+<script src="assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Sticky Kit -->
+<script src="assets/bower_components/sticky-kit/dist/sticky-kit.min.js"></script>
+
+<!-- Jarallax -->
+<script src="assets/bower_components/jarallax/dist/jarallax.min.js"></script>
+<script src="assets/bower_components/jarallax/dist/jarallax-video.min.js"></script>
+
+<!-- imagesLoaded -->
+<script src="assets/bower_components/imagesloaded/imagesloaded.pkgd.min.js"></script>
+
+<!-- Flickity -->
+<script src="assets/bower_components/flickity/dist/flickity.pkgd.min.js"></script>
+
+<!-- Photoswipe -->
+<script src="assets/bower_components/photoswipe/dist/photoswipe.min.js"></script>
+<script src="assets/bower_components/photoswipe/dist/photoswipe-ui-default.min.js"></script>
+
+<!-- Jquery Form -->
+<script src="assets/bower_components/jquery-form/dist/jquery.form.min.js"></script>
+
+<!-- Jquery Validation -->
+<script src="assets/bower_components/jquery-validation/dist/jquery.validate.min.js"></script>
+
+<!-- Jquery Countdown + Moment -->
+<script src="assets/bower_components/jquery.countdown/dist/jquery.countdown.min.js"></script>
+<script src="assets/bower_components/moment/min/moment.min.js"></script>
+<script src="assets/bower_components/moment-timezone/builds/moment-timezone-with-data.js"></script>
+
+<!-- Hammer.js -->
+<script src="assets/bower_components/hammer.js/hammer.min.js"></script>
+
+<!-- NanoSroller -->
+<script src="assets/bower_components/nanoscroller/bin/javascripts/jquery.nanoscroller.min.js"></script>
+
+<!-- SoundManager2 -->
+<script src="assets/bower_components/SoundManager2/script/soundmanager2-nodebug-jsmin.js"></script>
+
+<!-- Seiyria Bootstrap Slider -->
+<script src="assets/bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js"></script>
+
+<!-- Summernote -->
+<script src="assets/bower_components/summernote/dist/summernote.min.js"></script>
+
+<!-- nK Share -->
+<script src="assets/plugins/nk-share/nk-share.js"></script>
+
+<!-- Prism -->
+<script src="assets/bower_components/prism/prism.js"></script>
+
+<!-- GoodGames -->
+<script src="assets/js/goodgames.min.js"></script>
+<script src="assets/js/goodgames-init.js"></script>
+<!-- END: Scripts -->
+
+
+    
+</body>
+
+<!-- Mirrored from html.nkdev.info/goodgames/store.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 10 May 2017 05:48:00 GMT -->
+</html>
